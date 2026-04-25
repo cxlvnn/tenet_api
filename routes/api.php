36610673463414ebbc062f3e9ddc19067a6fc7a6
cxlvnn <behruzturabekov19@gmail.com>
2026-04-26 00:15:01 +0500

    fixed the information disclosure vulnerability (duh)

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,15 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register', [SessionController::class, 'register'])->name('register')->middleware('guest');
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [SessionController::class, 'register'])->name('register');
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/products/{product}', [ProductController::class, 'show']);
+    Route::get('/products/{product}', [ProductController::class, 'show'])->missing(function () {
+        return response()->json(['message' => 'Not Found'], 404);
+    });
     Route::post('/products', [ProductController::class, 'store']);
     Route::patch('/products/{product}', [ProductController::class, 'update']);
     Route::delete('/products/{product}', [ProductController::class, 'destroy']);
